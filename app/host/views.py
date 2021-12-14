800225e851ead72784acdf1c904d05f130e673ac
astrophpeter <petermcgill94@gmail.com>
2021-12-13 16:43:12 -0800

    fixed hips2fits error, keep cutout size to 0.1 degrees, more than that causes a time out on their server

--- app/host/views.py
+++ app/host/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import TransientForm
 from astropy.coordinates import SkyCoord
-from .host_utils import download_image_data, survey_list, construct_all_apertures
+from .host_utils import survey_list, construct_all_apertures
+from .cutouts import download_image_data
 from.plotting_utils import plot_image_grid
 
 def submit_transient(request):
@@ -10,10 +11,10 @@ def submit_transient(request):
         form = TransientForm(request.POST)
         if form.is_valid():
             ra, dec = form.cleaned_data['ra'], form.cleaned_data['dec']
-            survey = survey_list('host/survey_metadata.yml')
+            survey = survey_list('host/data/survey_metadata.yml')
             images = download_image_data(SkyCoord(ra=ra, dec=dec, unit='deg'), survey)
-            apertures = construct_all_apertures(SkyCoord(ra=ra, dec=dec, unit='deg'), images)
-            bokeh_cutout_dict = plot_image_grid(images, apertures)
+            #apertures = construct_all_apertures(SkyCoord(ra=ra, dec=dec, unit='deg'), images)
+            bokeh_cutout_dict = plot_image_grid(images, apertures=None)#apertures)
             all_surveys = [sur.name for sur in survey]
             missing_surveys = list(set(all_surveys) - images.keys())
             bokeh_cutout_dict.update({'missing_data': missing_surveys})
