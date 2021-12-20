ef4a2de5dde100e1b269316d5329c411734c454d
astrophpeter <petermcgill94@gmail.com>
2021-12-20 09:18:11 -0800

    added SED catalog

--- app/host/views.py
+++ app/host/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .forms import TransientForm
 from astropy.coordinates import SkyCoord
 from .host_utils import survey_list, construct_all_apertures
+from .catalog_photometry import download_catalog_data
 from .cutouts import download_image_data
-from.plotting_utils import plot_image_grid
+from.plotting_utils import plot_image_grid, plot_catalog_sed
 
 def submit_transient(request):
 
@@ -11,13 +12,19 @@ def submit_transient(request):
         form = TransientForm(request.POST)
         if form.is_valid():
             ra, dec = form.cleaned_data['ra'], form.cleaned_data['dec']
-            survey = survey_list('host/data/survey_metadata.yml')
-            images = download_image_data(SkyCoord(ra=ra, dec=dec, unit='deg'), survey)
-            apertures = construct_all_apertures(SkyCoord(ra=ra, dec=dec, unit='deg'), images)
-            bokeh_cutout_dict = plot_image_grid(images, apertures=apertures)
-            all_surveys = [sur.name for sur in survey]
-            missing_surveys = list(set(all_surveys) - images.keys())
-            bokeh_cutout_dict.update({'missing_data': missing_surveys})
+            #survey = survey_list('host/data/survey_metadata.yml')
+            catalog = survey_list('host/data/catalog_metadata.yml')
+            position = SkyCoord(ra=ra, dec=dec, unit='deg')
+            catalog_data = download_catalog_data(position, catalog)
+            #images = download_image_data(SkyCoord(ra=ra, dec=dec, unit='deg'), survey)
+            #apertures = construct_all_apertures(SkyCoord(ra=ra, dec=dec, unit='deg'), images)
+            #bokeh_cutout_dict = plot_image_grid(images, apertures=apertures)
+            bokeh_cutout_dict = plot_catalog_sed(catalog_data)
+
+            #all_surveys = [sur.name for sur in survey]
+            #missing_surveys = list(set(all_surveys) - images.keys())
+
+            #bokeh_cutout_dict.update({'missing_data': missing_surveys})
             return render(request, 'results.html', bokeh_cutout_dict)
 
 
