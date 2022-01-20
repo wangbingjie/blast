542630c52b1f6414d8ec8f3dc4bcacc624d4a040
Peter <petermcgill94@gmail.com>
2022-01-19 17:25:57 -0800

    started getting tns data ingestion working

--- app/host/transient_name_server.py
+++ app/host/transient_name_server.py
@@ -1,6 +1,9 @@
 # Download and ingest transients from the Transient Name Server (TNS)
 import os
-
+from collections import OrderedDict
+import json
+import requests
+from datetime import date
 
 def get_tns_credentials():
     """
@@ -16,13 +19,42 @@ def get_tns_credentials():
 
     return {credential: os.environ[credential] for credential in credentials}
 
+def search_tns(time_after, tns_config):
+    """
+    Queries TNS for transients with public timestamp > time_after.
+    """
+    search_obj = [("ra", ""), ("dec", ""), ("radius", ""), ("units", ""),
+                  ("objname", ""), ("internal_name", ""),
+                  ("public_timestamp", time_after.isoformat())]
+    search_url = tns_config['tns_api_url'] + "/search"
+    headers = {'User-Agent': tns_config['tns_marker']}
+    json_file = OrderedDict(search_obj)
+    search_data = {'api_key': tns_config['tns_bot_api_key'],
+                   'data': json.dumps(json_file)}
+    return requests.post(search_url, headers=headers, data=search_data)
 
-
-def ingest_new_transients():
+def ingest_new_transients(sandbox=False):
     """
     Ingest new transients from the transient name server
     """
-    tns_credentials = get_tns_credentials()
+    bot = get_tns_credentials()
+    TNS_BOT_ID, TNS_BOT_NAME = bot['TNS_BOT_ID'], bot['TNS_BOT_NAME']
+    TNS_BOT_API_KEY = bot['TNS_BOT_API_KEY']
+
+    if sandbox:
+        tns_api_url = 'https://sandbox.wis-tns.org/api/get'
+    else:
+        tns_api_url = 'https://www.wis-tns.org/api/get'
+
+    tns_marker = (f'tns_marker{{\"tns_id\": {TNS_BOT_ID},'
+                  f'\"type\": \"bot\", \"name\": \"{TNS_BOT_NAME}\"}}')
+
+    config = {'tns_marker': tns_marker, 'tns_bot_api_key': TNS_BOT_API_KEY,
+              'tns_api_url': tns_api_url}
+    search_results = search_tns(date(2022, 1, 1), config)
+
+    print(type(search_results.text))
 
     return None
 
+ingest_new_transients()
