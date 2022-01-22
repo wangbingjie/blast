bfd04ea258e6e5a28e0b74e03a2a3682e850a3dc
Peter <petermcgill94@gmail.com>
2022-01-21 17:53:48 -0800

    got celery beat working

--- app/host/tasks.py
+++ app/host/tasks.py
@@ -1,10 +1,17 @@
+from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from host.transient_name_server import ingest_new_transients
+import datetime
 
-@shared_task
-def test_task(x,y):
-    return x + y
-
-@shared_task
+@shared_task()
 def ingest_recent_tns_data():
+    time_after = datetime.datetime.now() - datetime.timedelta(hours=3)
+    ingest_new_transients(time_after)
+
+
+
+
+
+
 
 
