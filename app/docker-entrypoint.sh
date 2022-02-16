487601834e7c3b7faa963ac6a891d0e0ea29269b
astrophpeter <petermcgill94@gmail.com>
2022-02-15 16:39:31 -0800

    got mysql dockerise and working

---
+++ app/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+python manage.py makemigrations &&
+python manage.py migrate &&
+python manage.py loaddata setup_survey_data.yaml &&
+python manage.py loaddata setup_filter_data.yaml &&
+python manage.py loaddata setup_catalog_data.yaml &&
+python manage.py loaddata setup_test_transient.yaml &&
+python manage.py runserver 0.0.0.0:8000
