5d7cc0e47d2a7a39338b4f32fafa5471d87af0a6
astrophpeter <petermcgill94@gmail.com>
2022-02-17 14:51:56 -0800

    got nginx to work

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-emj*+kbyrdii%75%z_3@$wizbabb3vgwmyrn)0gsjgtui6ssha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'app/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 python manage.py makemigrations &&
 python manage.py migrate &&
-python manage.py loaddata setup_survey_data.yaml &&
-python manage.py loaddata setup_filter_data.yaml &&
-python manage.py loaddata setup_catalog_data.yaml &&
-python manage.py loaddata setup_test_transient.yaml &&
-gunicorn --bind 0.0.0.0:8000 app.wsgi
+#python manage.py loaddata setup_survey_data.yaml &&
+#python manage.py loaddata setup_filter_data.yaml &&
+#python manage.py loaddata setup_catalog_data.yaml &&
+#python manage.py loaddata setup_test_transient.yaml &&
+gunicorn app.wsgi:application --bind 0.0.0.0:8000
 #python manage.py runserver 0.0.0.0:8000
