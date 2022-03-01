73c308ff623fa23c6c028d8f3a39eefded7ad80e
astrophpeter <petermcgill94@gmail.com>
2022-03-01 10:46:01 -0800

    added more tests and updated docs

---
+++ app/host/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+class ViewTest(TestCase):
+    fixtures = ['setup_test_transient.yaml']
+
+
+    def test_transient_list_page(self):
+        response = self.client.get('/transients/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_transient_page(self):
+        response = self.client.get('/transients/2022testone/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/transients/2022testtwo/')
+        self.assertEqual(response.status_code, 200)
