e598cc798a7c1ac8ffc4536c3398930d551db6a6
astrophpeter <petermcgill94@gmail.com>
2022-03-08 15:08:33 -0800

    trying to get ci to work

--- app/app/settings.py
+++ app/app/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("MYSQL_DATABASE", ""),
+        "NAME": os.environ.get("MYSQL_DATABASE", "django"),
         "USER": "root",
         "PASSWORD": "password",
         "HOST": "database",
