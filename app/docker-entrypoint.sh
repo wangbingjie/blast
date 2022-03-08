31b50b9d2303e681dc8b86bb265878d0f28d399d
astrophpeter <petermcgill94@gmail.com>
2022-03-08 13:26:37 -0800

    fixing CI

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+
+bash ./wait-for-it.sh database:3306 --timeout=0 &&
 python manage.py makemigrations &&
 python manage.py migrate &&
 python manage.py loaddata setup_survey_data.yaml &&
