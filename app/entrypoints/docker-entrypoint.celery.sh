205000e7497b1cc7db4b024ffd7b2855ac094066
Peter <petermcgill94@gmail.com>
2022-03-10 02:17:50 +0000

    refactors entry points

--- app/entrypoints/docker-entrypoint.celery.sh
+++ app/entrypoints/docker-entrypoint.celery.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
-bash entrypoints/wait-for-it.sh database:3306 rabbitmq:5672 --timeout=0 &&
+bash entrypoints/wait-for-it.sh database:3306 --timeout=0 &&
+bash entrypoints/wait-for-it.sh rabbitmq:5672 --timeout=0 &&
+bash entrypoints/wait-for-it.sh app:8000 --timeout=0 &&
 celery -A app worker -l INFO --max-memory-per-child 12000
 
--- app/entrypoints/docker-entrypoint.sh
+++ app/entrypoints/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
-bash entrypoints/wait-for-it.sh database:3306 nginx:80 --timeout=0 &&
+bash entrypoints/wait-for-it.sh database:3306 --timeout=0 &&
+bash entrypoints/wait-for-it.sh nginx:80 --timeout=0 &&
 python manage.py makemigrations &&
 python manage.py migrate &&
 python manage.py loaddata setup_survey_data.yaml &&
