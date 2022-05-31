a55a07da74879c8c736ccc920b87fc11fb4af2d2
David Jones <david@eduroam-169-233-194-44.ucsc.edu>
2022-05-31 14:22:42 -0700

    a couple new utilities, including mJy

--- app/host/photometric_calibration.py
+++ app/host/photometric_calibration.py
@@ -6,11 +6,23 @@ def ab_mag_to_jansky(ab_mag):
     """
     return 10.0 ** (-(ab_mag + 48.60) / 2.5)
 
+def ab_mag_to_mJy(ab_mag):
+    """
+    Converts AB magnitude to spectral flux density in units of microJanskys.
+    """
+    return 10.0 ** (-(ab_mag + 23.90) / 2.5)
+
 def flux_to_mag(flux, zero_point_mag):
     """
     Converts flux to magnitude
     """
     return -2.5 * np.log10(flux) + zero_point_mag
+
+def flux_to_mJy_flux(flux, zero_point_mag_in):
+    """
+    Converts flux to magnitude
+    """
+    return 10**(-0.4*(zero_point_mag_in - 23.9))
 
 def counts_to_flux(counts, exposure_time):
     """
@@ -23,7 +35,6 @@ def jansky_to_maggies(flux_density_jansky):
     Converts spectral flux density from Janskys to units of maggies.
     """
     return flux_density_jansky / 3631.0
-
 
 def magnitude_to_flux_density(magnitude, survey):
     return 0.0
