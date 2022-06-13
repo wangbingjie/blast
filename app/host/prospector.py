a1fe82f0469446909708dc7d81f7203663348b8a
astrophpeter <petermcgill94@gmail.com>
2022-06-13 15:08:01 -0700

    continued to add prospector and refactored and tidied up fixtures

--- app/host/prospector.py
+++ app/host/prospector.py
@@ -1,14 +1,48 @@
 # Utils and wrappers for the prospector SED fitting code
+from .models import Filter, Transient, AperturePhotometry
+from sedpy.observate import load_filters
+from .photometric_calibration import jansky_to_maggies
+import numpy as np
 
+def build_obs(transient, aperture_type):
 
-def build_obs(my, arguments):
     """
     This functions is required by prospector and should return
     a dictionary defined by
     https://prospect.readthedocs.io/en/latest/dataformat.html.
 
     """
-    return 0.0
+
+    photometry = AperturePhotometry.objects.filter(transient=transient,
+                                      aperture__type__exact=aperture_type)
+
+    if not photometry.exists():
+        raise ValueError(f"No host photometry of type {aperture_type}")
+
+    if transient.host is None:
+        raise ValueError("No host galaxy match")
+
+    if transient.host.redshift is None:
+        raise ValueError("No host galaxy redshift")
+
+    filter_names, flux_maggies, flux_maggies_error = [], [], []
+
+    for filter in Filter.objects.all():
+        try:
+            datapoint = photometry.get(filter=filter)
+            filter_names.append(datapoint.kcorrect_name)
+            flux_maggies.append(jansky_to_maggies(datapoint.flux))
+            flux_maggies_error.append(jansky_to_maggies(datapoint.flux_error))
+        except AperturePhotometry.DoesNotExist or AperturePhotometry.MultipleObjectsReturned:
+            raise
+
+    obs_data = dict(wavelength=None, spectrum=None, unc=None,
+                    redshift=transient.host.redshift,
+                    maggies=np.array(flux_maggies),
+                    maggies_unc=np.array(flux_maggies_error),
+                    filters=load_filters(filter_names))
+
+    return obs_data
 
 
 def build_model(my, arguments):
