ec257c6561d645575757a73f82d5ed55fa826bd7
astrophpeter <petermcgill94@gmail.com>
2022-06-14 15:10:31 -0700

    tidying up fixutures and example data

--- app/entrypoints/docker-entrypoint.app.sh
+++ app/entrypoints/docker-entrypoint.app.sh
@@ -5,13 +5,14 @@ bash entrypoints/wait-for-it.sh nginx:80 --timeout=0 &&
 python manage.py makemigrations &&
 python manage.py migrate &&
 bash entrypoints/load_initial_data.sh &&
-python manage.py loaddata setup_survey_data.yaml &&
-python manage.py loaddata setup_filter_data.yaml &&
-python manage.py loaddata setup_catalog_data.yaml &&
-python manage.py loaddata setup_test_host.yaml &&
-python manage.py loaddata setup_test_transient.yaml &&
-python manage.py loaddata setup_test_cutout.yaml &&
-python manage.py loaddata setup_tasks.yaml &&
-python manage.py loaddata setup_status.yaml &&
-python manage.py loaddata setup_test_cutout.yaml &&
+bash entrypoints/load_example_data.sh &&
+#python manage.py loaddata setup_survey_data.yaml &&
+#python manage.py loaddata setup_filter_data.yaml &&
+#python manage.py loaddata setup_catalog_data.yaml &&
+#python manage.py loaddata setup_test_host.yaml &&
+#python manage.py loaddata setup_test_transient.yaml &&
+#python manage.py loaddata setup_test_cutout.yaml &&
+#python manage.py loaddata setup_tasks.yaml &&
+#python manage.py loaddata setup_status.yaml &&
+#python manage.py loaddata setup_test_cutout.yaml &&
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
