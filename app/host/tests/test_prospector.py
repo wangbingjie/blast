2851242f34566ba4721dde045413affb53d1fec1
astrophpeter <petermcgill94@gmail.com>
2022-06-14 10:20:09 -0700

    added transmission curves for sedpy

---
+++ app/host/tests/test_prospector.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from ..prospector import build_obs
+
+class PropsectorBuildObsTest(TestCase):
+    fixtures = [
+        'setup_filter_data.yaml'
+
+    ]
+
+    def test_build_obs(self):
+        pass
+
+
+
+
