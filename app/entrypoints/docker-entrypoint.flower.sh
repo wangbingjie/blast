f488790d77742196bab06beccf2aab56f86d30dc
Peter <petermcgill94@gmail.com>
2022-07-26 17:33:27 -0700

    fixed flower

--- app/entrypoints/docker-entrypoint.flower.sh
+++ app/entrypoints/docker-entrypoint.flower.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
-bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_PORT}:${MESSAGE_BROKER_PORT} --timeout=0 &&
+bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0 &&
-celery --broker=amqp://guest:guest@rabbitmq:${MESSAGE_BROKER_PORT}// flower --port=${FLOWER_PORT}
+celery --broker=amqp://${RABBITMQ_USERNAME}:${RABBITMQ_PASSWORD}@${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT}// flower --port=8888
