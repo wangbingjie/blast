d8e947f2207c0c7dccb71eb47fc6b6e6ee975176
Peter <petermcgill94@gmail.com>
2022-07-27 11:39:49 -0700

    refactored docker compose files

--- run/blast.run.local.sh
+++ run/blast.run.local.sh
@@ -1,3 +1,8 @@
 #!/bin/env bash
 bash app/entrypoints/clean_data.sh
-docker compose -f docker/docker-compose.yml --env-file env/.env.dev up --build
+
+case "$1" in
+  test) docker compose --profile $1 -f docker/docker-compose.yml --env-file env/.env.dev up --build --exit-code-from app_test;;
+    ci) docker compose --profile "test" -f docker/docker-compose.yml --env-file env/.env.ci up --build --exit-code-from app_test;;
+     *) docker compose --profile $1 -f docker/docker-compose.yml --env-file env/.env.dev up --build;;
+esac
