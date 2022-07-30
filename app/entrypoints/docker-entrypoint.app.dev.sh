b5fe9a6c91d8541f76861e8a0d34b9cc31f02e53
Peter <petermcgill94@gmail.com>
2022-07-30 11:32:01 -0700

    updated run to be served by runnserver so real-time code changes can be seen. Update docs, added text to todocs following djs suggestions

---
+++ app/entrypoints/docker-entrypoint.app.dev.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
+python manage.py makemigrations &&
+python manage.py migrate &&
+python manage.py createsuperuser --noinput &&
+python manage.py collectstatic &&
+bash entrypoints/load_initial_data.sh &&
+./manage.py shell < entrypoints/setup_initial_periodic_tasks.py &&
+bash entrypoints/load_example_data.sh &&
+python manage.py runserver 0.0.0.0:${WEB_APP_PORT}
