82867777bc3e4fef6d163b86489c6388f75cb404
Peter <petermcgill94@gmail.com>
2022-07-31 17:21:53 -0700

    adding ignored files

--- app/entrypoints/docker-entrypoint.app.test.sh
+++ app/entrypoints/docker-entrypoint.app.test.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations &&
 python manage.py migrate &&
 coverage run manage.py test host.tests -v 2 &&
 coverage report -i --omit=host/tests/*,host/migrations/*,app/*,host/urls.py,host/admin.py,host/apps.py,host/__init__.py,manage.py &&
-coverage xml -i --omit=host/tests/*,host/migrations/*,app/*,host/urls.py,host/admin.py,host/apps.py,host/__init__.py,manage.py
+coverage xml -i
 #python manage.py test host.tests -v 2
