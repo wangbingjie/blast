df8c12aa6104b0c40183246740b7478a7248cb3c
Peter <petermcgill94@gmail.com>
2022-08-11 10:23:34 -0700

    api working

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,11 +3,12 @@ import os
 from django.urls import path
 
 from . import views
+from . import serializers
 
 base_path = os.environ.get("BASE_PATH", "").strip("/")
 if base_path != "":
     base_path = f"""{base_path}/"""
 
 urlpatterns = [
-    path(f"""{base_path}<slug:slug>""", views.api_info)
+    path(f"""{base_path}transient/<slug:slug>""", views.transient_data)
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from . import serializers
+from host.models import Transient
 
 
 @api_view(["Get"])
-def api_info(request, slug):
-    return Response({"Hello": {"slug": slug}})
+def transient_data(request, slug):
+
+    try:
+        transient = Transient.objects.get(name__exact=slug)
+        data = serializers.serialize_transient_data(transient)
+    except:
+        data = {"Transient not in blast database"}
+
+    return Response(data)
