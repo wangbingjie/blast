94fdc7b3ec6e6b752e4ad9d449e55248e02fe622
Peter <petermcgill94@gmail.com>
2022-08-11 17:41:41 -0700

    api is forming

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,18 +1,44 @@
-from host.models import Transient
+from host import models
+from rest_framework import serializers
 
 
-def serialize_transient_data(transient) -> dict:
+class TransientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Transient
+        fields = ['name', 'ra_deg', 'dec_deg', 'public_timestamp', 'redshift',
+                  'milkyway_dust_reddening', 'spectroscopic_class',
+                  'photometric_class']
+
+
+class HostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Host
+        fields = ['name', 'ra_deg', 'dec_deg', 'redshift',
+                  'milkyway_dust_reddening']
+
+
+class ApertureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Aperture
+        fields = ['orientation_deg', 'semi_major_axis_arcsec',
+                  'semi_minor_axis_arcsec']
+
+def serialize_blast_science_data(transient) -> dict:
     """
     Serializes all data associated with a transient
     """
+    transient_data = TransientSerializer(transient).data
+    host_data = HostSerializer(transient.host).data
 
-    return {"transient_name": transient.name,
-            "transient_ra": transient.ra_deg,
-            "transient_dec": transient.dec_deg,
-            "host_name": transient.host.name,
-            "host_ra": transient.host.ra_deg,
-            "host_dec": transient.host.dec_deg,
-            }
+    for filter in models.Filter.objects.all():
+        pass
+
+    transient_data = {'transient_' + name: value for name, value in transient_data.items()}
+    host_data = {'host_' + name: value for name, value in host_data.items()}
+    return {**transient_data, **host_data}
+
+
+
 
 
 
