2f3f656eb420fe79c1b9239cd4aa67422bb46b29
Peter <petermcgill94@gmail.com>
2022-08-14 13:56:22 -0700

    continuing with api dev

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -2,6 +2,11 @@ from host import models
 from rest_framework import serializers
 
 
+class CutoutField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.filter.name
+
+
 class TransientSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Transient
@@ -17,25 +22,57 @@ class HostSerializer(serializers.ModelSerializer):
                   'milkyway_dust_reddening']
 
 
+class ApertureSerializer(serializers.ModelSerializer):
+    cutout = CutoutField(read_only=True)
+
+    class Meta:
+        model = models.Aperture
+        fields = ['orientation_deg', 'semi_major_axis_arcsec',
+                  'semi_minor_axis_arcsec', 'type', 'cutout']
+
+
 class AperturePhotometrySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.AperturePhotometry
         fields = ['flux', 'flux_error', 'magnitude', 'magnitude_error']
 
 
-def serialize_blast_science_data(transient) -> dict:
+def serialize_blast_science_data(transient_name) -> dict:
     """
     Serializes all data associated with a transient
     """
-    transient_data = TransientSerializer(transient).data
-    host_data = HostSerializer(transient.host).data
+    serializers = [TransientSerializer, HostSerializer, ApertureSerializer,
+                   ApertureSerializer]
+    prefixes = ['transient_', 'host_', 'local_aperture_', 'global_aperture_']
+    queries = [{"name__exact": transient_name}, {"transient__name__exact": transient_name},
+               {"transient__name__exact": transient_name, "type__exact": "local"},
+               {"transient__name__exact": transient_name, "type__exact": "global"}]
+    blast_models = [models.Transient, models.Host, models.Aperture, models.Aperture]
 
     for filter in models.Filter.objects.all():
-        pass
+        for type in ['local', 'global']:
+            serializers.append(AperturePhotometrySerializer)
+            blast_models.append(models.AperturePhotometry)
+            queries.append({"transient__name__exact": transient_name,
+                            "filter__name__exact": filter.name,
+                            "aperture__type__exact": type})
+            prefixes.append(f"{type}_{filter.name}_")
 
-    transient_data = {'transient_' + name: value for name, value in transient_data.items()}
-    host_data = {'host_' + name: value for name, value in host_data.items()}
-    return {**transient_data, **host_data}
+    science_payload = {}
+
+    for model, serializer, prefix, query in zip(blast_models, serializers, prefixes, queries):
+        try:
+            object = model.objects.get(**query)
+            object_data = serializer(object).data
+            object_dict = {prefix + name: value for name, value in object_data.items()}
+
+        except:
+            object_dict = {prefix + name: None for name in serializer().fields}
+
+        science_payload = {**science_payload, **object_dict}
+
+    return science_payload
+
 
 
 
