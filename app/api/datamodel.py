b11e2b8ff00e09133e28234f52870e43df88a38d
Peter <petermcgill94@gmail.com>
2022-08-15 14:14:21 -0700

    updated api

---
+++ app/api/datamodel.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from host import models
+from rest_framework import serializers
+from django.db import models
+
+@dataclass
+class DataModelComponent:
+    prefix: str
+    query: dict
+    model: models.Model
+    serializer: serializers.Serializer
+
+
+def serialize_blast_science_data(datamodel) -> dict:
+    """
+    Serializes all data associated with a transient into a flat structure.
+    """
+    science_payload = {}
+    for component in datamodel:
+        prefix, serializer = component.prefix, component.serializer
+        try:
+            object = component.model.objects.get(**component.query)
+            object_data = serializer(object).data
+            object_dict = {prefix + name: value for name, value in object_data.items()}
+        except:
+            object_dict = {prefix + name: None for name in serializer().fields}
+
+        science_payload = {**science_payload, **object_dict}
+    return science_payload
--- app/api/views.py
+++ app/api/views.py
@@ -1,11 +1,16 @@
 from host.models import Transient
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import itertools
 
-from . import serializers
+from . import datamodel
+from .components import data_model_components
 
 
 @api_view(["Get"])
 def transient_data(request, slug):
-    data = serializers.serialize_blast_science_data(slug)
+    component_groups = [component_group(slug) for component_group in data_model_components]
+    # flatten the component groups
+    components = list(itertools.chain(component_groups))
+    data = datamodel.serialize_blast_science_data(components)
     return Response(data)
