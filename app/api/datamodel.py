7a9fdebc2ebaca3c0dad90620ab4e8423bc72c9b
Peter <petermcgill94@gmail.com>
2022-08-19 16:45:55 -0700

    added further tests

--- app/api/datamodel.py
+++ app/api/datamodel.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import List
+import itertools
 
 import django
 from host import models
@@ -29,3 +31,19 @@ def serialize_blast_science_data(datamodel) -> dict:
 
         science_payload = {**science_payload, **object_dict}
     return science_payload
+
+
+def unpack_component_groups(component_groups) -> List[DataModelComponent]:
+    """
+    Unpacks list of datamodel component groups into a flat strcuture
+
+    paramters:
+        component_groups (List[DataModelComponents]): list of grops to be unpacked
+    returns:
+        flat_components (List[DataModelComponents]): flattened list of components
+    """
+    is_nested = any(isinstance(item, list) for item in component_groups)
+    flat_list = list(itertools.chain(*component_groups)) if is_nested else component_groups
+    return flat_list
+
+
