ac893b94e3c1bf429400a4b67cbfd762b9549dcd
Peter <petermcgill94@gmail.com>
2022-08-19 15:06:33 -0700

    testing the api

---
+++ app/api/tests/test_datamodel.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from host import models
+
+from .. import serializers
+from ..datamodel import serialize_blast_science_data
+from ..components import host_component
+from ..components import transient_component
+
+class DatamodelConstructionTest(TestCase):
+    fixtures = ["../fixtures/test/filters.yaml", "../fixtures/test/test_transient.yaml"]
+
+    def test_datamodel_build(self):
+        host = host_component("2022testone")
+        transient = transient_component("2022testone")
+        data = serialize_blast_science_data(host+transient)
+        self.assertTrue(type(data) is dict)
