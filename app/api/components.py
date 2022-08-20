ea8df72eb33a0dbd83fe135db065874a65991feb
Peter <petermcgill94@gmail.com>
2022-08-20 13:51:05 -0700

    doc strings

--- app/api/components.py
+++ app/api/components.py
@@ -1,5 +1,10 @@
-from typing import List
+"""
+This module contains definitions of the science payload components. Functions
+that turn blast database scheme into a flat science payload to be served through
+the API.
+"""
 
+from typing import List
 from host import models
 
 from . import serializers
@@ -7,6 +12,17 @@ from .datamodel import DataModelComponent
 
 
 def transient_component(transient_name) -> List[DataModelComponent]:
+    """
+    Transient model component defined what transient information is in the
+    science payload.
+
+    parameters:
+        transient_name (str): name of the transient.
+    returns:
+        component (List[DataModelComponent]): data model component to be added
+            to the blast science payload.
+
+    """
     component = DataModelComponent(
         prefix="transient_",
         query={"name__exact": transient_name},
@@ -17,6 +33,16 @@ def transient_component(transient_name) -> List[DataModelComponent]:
 
 
 def host_component(transient_name) -> List[DataModelComponent]:
+    """
+    Host model component defined what transient information is in the
+    science payload.
+
+    parameters:
+        transient_name (str): name of the transient.
+    returns:
+        component (List[DataModelComponent]): data model component to be added
+            to the blast science payload.
+    """
     component = DataModelComponent(
         prefix="host_",
         query={"transient__name__exact": transient_name},
@@ -27,6 +53,16 @@ def host_component(transient_name) -> List[DataModelComponent]:
 
 
 def aperture_component(transient_name) -> List[DataModelComponent]:
+    """
+    Apeture model component defined what transient information is in the
+    science payload.
+
+    parameters:
+        transient_name (str): name of the transient.
+    returns:
+        component (List[DataModelComponent]): data model component to be added
+            to the blast science payload.
+    """
     components = []
     for aperture_type in ["local", "global"]:
         components.append(
@@ -45,6 +81,16 @@ def aperture_component(transient_name) -> List[DataModelComponent]:
 
 
 def photometry_component(transient_name) -> List[DataModelComponent]:
+    """
+    Photometry model component defined what transient information is in the
+    science payload.
+
+    parameters:
+        transient_name (str): name of the transient.
+    returns:
+        component (List[DataModelComponent]): data model component to be added
+            to the blast science payload.
+    """
     components = []
     filters = models.Filter.objects.all()
     for aperture_type in ["local", "global"]:
