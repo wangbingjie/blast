038c5db49fc9c7dc854f3f1d0ed5f85f4011095f
astrophpeter <petermcgill94@gmail.com>
2022-10-21 14:58:26 -0700

    plotting entire jones sample

---
+++ app/host/cutout_images.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from astropy.io import fits
+
+
+def panstarrs_image_filename(position, image_size=None, filter=None):
+    """Query panstarrs service to get a list of image names
+
+    Parameters
+    ----------
+    :position : :class:`~astropy.coordinates.SkyCoord`
+        Target centre position of the cutout image to be downloaded.
+    :size : int: cutout image size in pixels.
+    :filter: str: Panstarrs filter (g r i z y)
+    Returns
+    -------
+    :filename: str: file name of the cutout
+    """
+
+    service = "https://ps1images.stsci.edu/cgi-bin/ps1filenames.py"
+    url = (
+        f"{service}?ra={position.ra.degree}&dec={position.dec.degree}"
+        f"&size={image_size}&format=fits&filters={filter}"
+    )
+
+    filename_table = pd.read_csv(url, delim_whitespace=True)["filename"]
+    return filename_table[0] if len(filename_table) > 0 else None
+
+
+def panstarrs_cutout(position, image_size=None, filter=None):
+    """
+    Download Panstarrs cutout from their own service
+
+    Parameters
+    ----------
+    :position : :class:`~astropy.coordinates.SkyCoord`
+        Target centre position of the cutout image to be downloaded.
+    :image_size: int: size of cutout image in pixels
+    :filter: str: Panstarrs filter (g r i z y)
+    Returns
+    -------
+    :cutout : :class:`~astropy.io.fits.HDUList` or None
+    """
+    filename = panstarrs_image_filename(position, image_size=image_size, filter=filter)
+    if filename is not None:
+        service = "https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
+        fits_url = (
+            f"{service}ra={position.ra.degree}&dec={position.dec.degree}"
+            f"&size={image_size}&format=fits&red={filename}"
+        )
+        fits_image = fits.open(fits_url)
+    else:
+        fits_image = None
+
+    return fits_image
+
--- validation/validation_scripts/plot_cutouts_matching.py
+++ validation/validation_scripts/plot_cutouts_matching.py
@@ -2,7 +2,56 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from astropy.coordinates import SkyCoord
 import numpy as np
+import sys
+from matplotlib.colors import PowerNorm
+from astropy.wcs import WCS
+from astropy.visualization import PercentileInterval, AsinhStretch
 
 sys.path.append("app/host")
+from cutout_images import panstarrs_cutout
 
-from cutouts import panstarrs_cutout
+results = pd.read_csv('validation_results/matching_ghost_jones+2018.csv')
+
+
+
+
+n = 6
+m = 45
+fig, axs = plt.subplots(m, n, figsize=(20, 252))
+
+for (_, match), ax in zip(list(results.iterrows())[:12], axs.reshape(-1)[:12]):
+    pred_ra, pred_dec = match['predicted_host_ra_deg'], match['predicted_host_dec_deg']
+    actual_ra, actual_dec = match['host_ra_deg'], match['host_dec_deg']
+    transient_ra, transient_dec = match['transient_ra_deg'], match['transient_dec_deg']
+    transient_name = match['transient_name']
+
+    transient_position = SkyCoord(ra=transient_ra, dec=transient_dec, unit="deg")
+    ghost_position = SkyCoord(ra=pred_ra, dec=pred_dec, unit="deg")
+    jones_position = SkyCoord(ra=actual_ra, dec=actual_dec, unit="deg")
+
+
+
+    image = panstarrs_cutout(transient_position, image_size=250, filter="g")
+    image_data = image[0].data
+    image_data[np.isnan(image_data)] = 0.0
+    # set contrast to something reasonable
+    transform = AsinhStretch() + PercentileInterval(99.5)
+    image_data = transform(image_data)
+
+    wcs = WCS(image[0].header)
+    ax.imshow(image_data, cmap='Greys', origin='lower')
+
+    sn_x, sn_y = transient_position.to_pixel(wcs)
+    ghost_x, ghost_y = ghost_position.to_pixel(wcs)
+    jones_x, jones_y = jones_position.to_pixel(wcs)
+
+    ax.scatter(sn_x, sn_y, marker="+", c="purple")
+    ax.scatter(ghost_x, ghost_y, label='GHOST', s=200, facecolors='none', edgecolors='r')
+    ax.scatter(jones_x, jones_y, facecolors='none', edgecolors='b', marker="^")
+
+    ax.set_title(transient_name)
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticks([])
+
+plt.tight_layout()
+plt.savefig("validation_plots/GHOST_jones18_match_mosaic.pdf", dpi=150)
