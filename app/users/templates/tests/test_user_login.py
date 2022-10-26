0158c6a6350ad85728d6cbe3d3dba605ad9a1cb2
astrophpeter <petermcgill94@gmail.com>
2022-10-25 17:32:43 -0700

    adding users test

---
+++ app/users/templates/tests/test_user_login.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/accounts/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
