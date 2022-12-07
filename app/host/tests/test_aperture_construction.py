ff251d47229803b87bacb00c498418d8272e61fa
David Jones <david.jones@ucsc.edu>
2022-12-07 13:50:10 -1000

    new test for NoneType segmentation map

--- app/host/tests/test_aperture_construction.py
+++ app/host/tests/test_aperture_construction.py
@@ -6,6 +6,11 @@ from ..models import TaskRegister
 from ..models import Transient
 from ..transient_tasks import GlobalApertureConstruction
 
+from ..host_utils import build_source_catalog
+from ..host_utils import estimate_background
+
+from astropy.io import fits
+import numpy as np
 
 class TestApertureConstruction(TestCase):
 
@@ -27,3 +32,14 @@ class TestApertureConstruction(TestCase):
         status_message = gac_cls._run_process(transient)
 
         assert status_message == "processed"
+
+    def test_aperture_failures(self):
+
+        data = np.zeros((500, 5000), dtype=np.float64)
+        hdu = fits.PrimaryHDU(data=data)
+        hdulist = fits.HDUList(hdus=[hdu])
+        
+        background = estimate_background(hdulist)
+        catalog = build_source_catalog(hdulist,background)
+        
+        assert catalog is None
