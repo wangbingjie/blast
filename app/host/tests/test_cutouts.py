193c4ed5dcad86e88c9cab287c2935df885a36cc
David Jones <david.jones@ucsc.edu>
2023-03-27 07:36:51 -1000

    test bug fix

--- app/host/tests/test_cutouts.py
+++ app/host/tests/test_cutouts.py
@@ -35,6 +35,6 @@ class CutoutDownloadTest(TestCase):
                 save_dir = f"{self.transient_name}/{filter.name}/"
                 os.makedirs(save_dir, exist_ok=True)
                 path_to_fits = save_dir + f"{filter.name}.fits"
-                fits.writeto(path_to_fits, overwrite=True)
+                fits.writeto(path_to_fits, cutout_data, overwrite=True)
 
         self.assertTrue(1 == 1)
