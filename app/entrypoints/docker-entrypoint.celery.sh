1f438a3836c720a6270deb34ab69cac4bc3d0467
David Jones <djones1741@gmail.com>
2024-02-25 13:10:27 -1000

    docker updates

--- app/entrypoints/docker-entrypoint.celery.sh
+++ app/entrypoints/docker-entrypoint.celery.sh
@@ -4,4 +4,5 @@ bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0 &&
 python manage.py shell < entrypoints/initialize_dustmaps.py &&
-celery -A app worker -l ERROR --max-memory-per-child 12000
+#celery -A app worker -l INFO --max-memory-per-child 12000 -P eventlet -c 500 -Ofair --prefetch-multiplier 1
+celery -A app worker -l DEBUG --max-memory-per-child 12000
--- app/entrypoints/docker-entrypoint.flower.sh
+++ app/entrypoints/docker-entrypoint.flower.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
+export FLOWER_UNAUTHENTICATED_API=true
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0 &&
