48bc355f734b1706d255b0134b6c8be161cb8e9a
David Jones <djones1741@gmail.com>
2024-02-25 21:05:53 -1000

    download sbi files at start to avoid space limits

--- app/entrypoints/docker-entrypoint.celery.sh
+++ app/entrypoints/docker-entrypoint.celery.sh
@@ -1,5 +1,4 @@
 #!/bin/env bash
-
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0 &&
