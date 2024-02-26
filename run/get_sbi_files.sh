de1cdb3eb46fa28b37f65de2f646db461bc29ae4
David Jones <djones1741@gmail.com>
2024-02-25 21:14:13 -1000

    SBI files bug fix

--- run/get_sbi_files.sh
+++ run/get_sbi_files.sh
@@ -1,14 +1,14 @@
 #!/bin/env bash
 
-if [[ -f sbi_phot/sbi_phot_global.h5 ]]
+if [[ -f data/sbipp_phot/sbi_phot_global.h5 ]]
 then
     echo "SBI/files already downloaded"
 else
   echo "downloading SBI files"
-  mkdir sbipp_phot
+  mkdir data/sbipp_phot
   curl -LJO https://zenodo.org/records/10703208/files/sbi_phot_global.h5 &&
   curl -LJO https://zenodo.org/records/10703208/files/sbi_phot_local.h5 &&
-  mv sbi_phot_global.h5 sbipp_phot/ &&
-  mv sbi_phot_local.h5 sbipp_phot/
+  mv sbi_phot_global.h5 data/sbipp_phot/ &&
+  mv sbi_phot_local.h5 data/sbipp_phot/
 fi
 
