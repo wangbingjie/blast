1b0c5de1d877cffeb2c0228a59b977a90282c5e7
T. Andrew Manning <manninga@illinois.edu>
2024-03-25 14:37:39 -0500

    Reduce init script runtime by only loading Python dependencies once

--- app/entrypoints/docker-entrypoint.app.sh
+++ app/entrypoints/docker-entrypoint.app.sh
@@ -1,12 +1,17 @@
-#!/bin/env bash
-bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
-bash entrypoints/wait-for-it.sh ${WEB_SERVER_HOST}:${WEB_SERVER_PORT} --timeout=0 &&
-python manage.py makemigrations &&
-python manage.py migrate &&
-python manage.py createsuperuser --noinput &&
-python manage.py collectstatic &&
-bash entrypoints/load_initial_data.sh &&
-./manage.py shell < entrypoints/setup_initial_periodic_tasks.py &&
-bash entrypoints/load_example_data.sh &&
-python manage.py shell < entrypoints/initialize_dustmaps.py &&
-gunicorn app.wsgi:application --bind 0.0.0.0:${WEB_APP_PORT}
+
+    set -eo pipefail
+
+    bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0
+    bash entrypoints/wait-for-it.sh ${WEB_SERVER_HOST}:${WEB_SERVER_PORT} --timeout=0
+
+    INIT_CHECK_FILE=/app/static/.initialized
+    if [[ ! -f "${INIT_CHECK_FILE}" ]]; then
+      echo "\"${INIT_CHECK_FILE}\" not found. Running initialization script..."
+      python init.py
+      touch "${INIT_CHECK_FILE}"
+    else
+      echo "Application already initialized (\"${INIT_CHECK_FILE}\" exists)."
+    fi
+
+    echo "Starting server..."
+    gunicorn app.wsgi --timeout 0 --bind 0.0.0.0:${WEB_APP_PORT} --workers=${GUNICORN_WORKERS:=1}
