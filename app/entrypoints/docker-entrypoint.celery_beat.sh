f59253fe633dd1bfb179854947ccff0f39476feb
T. Andrew Manning <manninga@illinois.edu>
2024-03-25 14:35:02 -0500

    Remove redundant initialize_dustmaps.py invocation

--- app/entrypoints/docker-entrypoint.celery_beat.sh
+++ app/entrypoints/docker-entrypoint.celery_beat.sh
@@ -3,5 +3,4 @@
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0 &&
 bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0 &&
-python manage.py shell < entrypoints/initialize_dustmaps.py &&
 celery -A app beat -l INFO
