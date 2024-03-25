e1a48d9061deab2ffdffd09a618a3836b9b7fd51
T. Andrew Manning <manninga@illinois.edu>
2024-03-25 14:19:06 -0500

    Parameterize Django SECRET_KEY and DEBUG in settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-emj*+kbyrdii%75%z_3@$wizbabb3vgwmyrn)0gsjgtui6ssha"
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-tn6@rg(#694!6p^c!^0ekz5d)jyxk(dxtx-z9m2%$h&w$p0#+)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "false").lower() == 'true'
 
 ALLOWED_HOSTS = ["*"]
 
