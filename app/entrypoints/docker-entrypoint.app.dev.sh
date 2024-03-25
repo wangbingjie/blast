4abb72da560744040bcab371c57211cb7c1cddfc
T. Andrew Manning <manninga@illinois.edu>
2024-03-25 14:34:23 -0500

    Cherry pick upstream changes 083ef3a7cf624e65313ae13ada301f81873ec4c1

--- app/entrypoints/docker-entrypoint.app.dev.sh
+++ app/entrypoints/docker-entrypoint.app.dev.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
+python manage.py shell < entrypoints/initialize_dustmaps.py &&
 python manage.py makemigrations &&
 python manage.py migrate &&
 python manage.py createsuperuser --noinput &&
@@ -7,5 +8,4 @@ python manage.py collectstatic &&
 bash entrypoints/load_initial_data.sh &&
 ./manage.py shell < entrypoints/setup_initial_periodic_tasks.py &&
 bash entrypoints/load_example_data.sh &&
-python manage.py shell < entrypoints/initialize_dustmaps.py &&
 python manage.py runserver 0.0.0.0:${WEB_APP_PORT}
