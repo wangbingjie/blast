0a196e660b985d5f34eb23997088383ce1da819a
T. Andrew Manning <manninga@illinois.edu>
2024-03-26 11:35:14 -0500

    Copy read-only .dustmapsrc into place and update initialize_dustmaps.py

--- app/entrypoints/docker-entrypoint.app.sh
+++ app/entrypoints/docker-entrypoint.app.sh
@@ -5,6 +5,12 @@ set -eo pipefail
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0
 bash entrypoints/wait-for-it.sh ${WEB_SERVER_HOST}:${WEB_SERVER_PORT} --timeout=0
 
+# If .dustmapsrc has been mounted as a read-only file at /tmp/.dustmapsrc,
+# copy it to the expected location /root/.dustmapsrc if it does not already exist.
+if [[ ! -f "/root/.dustmapsrc" && -f "/tmp/.dustmapsrc" ]]; then
+  cp /tmp/.dustmapsrc /root/.dustmapsrc
+fi
+
 INIT_CHECK_FILE=/app/static/.initialized
 if [[ ! -f "${INIT_CHECK_FILE}" ]]; then
   echo "\"${INIT_CHECK_FILE}\" not found. Running initialization script..."
