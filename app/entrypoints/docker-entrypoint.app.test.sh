4909f51ae0f96c67f4234cfd7de97f15d6f92558
T. Andrew Manning <manninga@illinois.edu>
2024-03-29 15:30:13 -0500

    Update continuous-integration GitHub workflow to use new data init scripts

--- app/entrypoints/docker-entrypoint.app.test.sh
+++ app/entrypoints/docker-entrypoint.app.test.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
+
+bash entrypoints/initialize_data_dirs.sh
+bash entrypoints/initialize_all_data.sh
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
-#python manage.py makemigrations &&
-#python manage.py migrate &&
 python manage.py shell < entrypoints/initialize_dustmaps.py &&
 coverage run manage.py test host.tests api.tests users.tests -v 2 &&
 coverage report -i --omit=host/tests/*,host/migrations/*,app/*,host/urls.py,host/admin.py,host/apps.py,host/__init__.py,manage.py &&
 coverage xml -i
-
