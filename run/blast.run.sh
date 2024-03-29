b4bf7a7f5ef0d27b32ca5d2ab02914dae8063ea4
T. Andrew Manning <manninga@illinois.edu>
2024-03-29 14:17:54 -0500

    Change to parent directory of run/blast.run.sh

--- run/blast.run.sh
+++ run/blast.run.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+cd "$(dirname "$(readlink -f "$0")")"/..
+
 case "$1" in
       test) docker compose --profile $1 --project-name blast -f docker/docker-compose.yml --env-file env/.env.dev up --build --exit-code-from app_test;;
         ci) docker compose --profile $1 --project-name blast -f docker/docker-compose.yml --env-file env/.env.ci up --build --exit-code-from app_ci;;
