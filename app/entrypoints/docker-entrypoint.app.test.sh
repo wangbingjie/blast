2ed3c986df1cc2b09fa3c4798497f1a47f3b59f7
T. Andrew Manning <manninga@illinois.edu>
2024-03-29 15:47:22 -0500

    Include .dustmapsrc init in CI pipeline script

--- app/entrypoints/docker-entrypoint.app.test.sh
+++ app/entrypoints/docker-entrypoint.app.test.sh
@@ -1,8 +1,18 @@
 #!/bin/env bash
 
+# Download and install required data
 bash entrypoints/initialize_data_dirs.sh
 bash entrypoints/initialize_all_data.sh
+
+# Wait for database
 bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
+
+# If .dustmapsrc has been mounted as a read-only file at /tmp/.dustmapsrc,
+# copy it to the expected location /root/.dustmapsrc if it does not already exist.
+if [[ ! -f "/root/.dustmapsrc" && -f "/tmp/.dustmapsrc" ]]; then
+  cp /tmp/.dustmapsrc /root/.dustmapsrc
+fi
+
 python manage.py shell < entrypoints/initialize_dustmaps.py &&
 coverage run manage.py test host.tests api.tests users.tests -v 2 &&
 coverage report -i --omit=host/tests/*,host/migrations/*,app/*,host/urls.py,host/admin.py,host/apps.py,host/__init__.py,manage.py &&
