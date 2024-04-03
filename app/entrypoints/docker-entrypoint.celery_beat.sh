0557e4a5582b7e7e13eead029497ae756789645b
T. Andrew Manning <manninga@illinois.edu>
2024-04-03 17:20:00 -0500

    Run initialize_data_dirs.sh in celery containers

--- app/entrypoints/docker-entrypoint.celery_beat.sh
+++ app/entrypoints/docker-entrypoint.celery_beat.sh
@@ -1,6 +1,17 @@
 #!/bin/env bash
 
-bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0 &&
-bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0 &&
-bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0 &&
+set -e
+
+bash entrypoints/wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=0
+bash entrypoints/wait-for-it.sh ${MESSAGE_BROKER_HOST}:${MESSAGE_BROKER_PORT} --timeout=0
+bash entrypoints/wait-for-it.sh ${WEB_APP_HOST}:${WEB_APP_PORT} --timeout=0
+
+# If .dustmapsrc has been mounted as a read-only file at /tmp/.dustmapsrc,
+# copy it to the expected location /root/.dustmapsrc if it does not already exist.
+if [[ ! -f "/root/.dustmapsrc" && -f "/tmp/.dustmapsrc" ]]; then
+  cp /tmp/.dustmapsrc /root/.dustmapsrc
+fi
+
+bash entrypoints/initialize_data_dirs.sh
+
 celery -A app beat -l INFO
