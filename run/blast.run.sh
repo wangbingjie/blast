55afe5c721a83948e5940b688ca6d32f96f56b56
T. Andrew Manning <manninga@illinois.edu>
2024-04-03 16:19:48 -0500

    Use hierarchical env files: required .env.default, optional override .env.dev

--- run/blast.run.sh
+++ run/blast.run.sh
@@ -3,7 +3,20 @@
 cd "$(dirname "$(readlink -f "$0")")"/..
 
 case "$1" in
-      test) docker compose --profile $1 --project-name blast -f docker/docker-compose.yml --env-file env/.env.dev up --build --exit-code-from app_test;;
-        ci) docker compose --profile $1 --project-name blast -f docker/docker-compose.yml --env-file env/.env.ci up --build --exit-code-from app_ci;;
-         *) docker compose --profile $1 --project-name blast -f docker/docker-compose.yml --env-file env/.env.dev up --build --abort-on-container-exit;;
+  test) DOCKER_ARGS="--exit-code-from app_test" ;;
+    ci) DOCKER_ARGS="--exit-code-from app_ci"   ;;
+     *) DOCKER_ARGS="--abort-on-container-exit" ;;
 esac
+
+EXTRA_ENV=""
+if [[ -f "env/.env.dev" ]]; then
+  EXTRA_ENV="--env-file env/.env.dev"
+fi
+
+set -x
+docker compose \
+  --profile $1 \
+  --project-name blast \
+  -f docker/docker-compose.yml \
+  --env-file env/.env.default ${EXTRA_ENV} \
+  up --build ${DOCKER_ARGS}
--- run/blast.stop.sh
+++ run/blast.stop.sh
@@ -9,12 +9,21 @@ if [[ "${PURGE}" == "--purge" ]]; then
   DOCKER_ARGS="--remove-orphans --volumes"
   echo "Purging Docker data volumes..."
   set -x
-  rm -f docker/initialized/.initialized
+  # rm -f docker/initialized/.initialized
+  # rm -f /mnt/data/.initializing_data
+  # rm -f /mnt/data/.initializing_db
   set +x
 fi
 
-docker compose --profile $1 \
-  -f docker/docker-compose.yml \
+EXTRA_ENV=""
+if [[ -f "env/.env.dev" ]]; then
+  EXTRA_ENV="--env-file env/.env.dev"
+fi
+
+set -x
+docker compose \
+  --profile $1 \
   --project-name blast \
-  --env-file env/.env.dev \
+  -f docker/docker-compose.yml \
+  --env-file env/.env.default ${EXTRA_ENV} \
   down ${DOCKER_ARGS}
