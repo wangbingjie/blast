3186551adf028e945f44df8a0e8d05f80845993d
T. Andrew Manning <manninga@illinois.edu>
2024-04-19 09:20:14 -0500

    Only create data symlinks if not present

--- app/entrypoints/initialize_data_dirs.sh
+++ app/entrypoints/initialize_data_dirs.sh
@@ -15,12 +15,32 @@ mkdir -p sbipp_phot
 
 mkdir -p /data/
 
-ln -sf /mnt/data/cutout_cdn   /data/cutout_cdn
-ln -sf /mnt/data/sed_output   /data/sed_output
-ln -sf /mnt/data/ghost_output /ghost_output
-ln -sf /mnt/data/tns_staging  /tns_staging
-ln -sf /mnt/data/transmission /transmission
-ln -sf /mnt/data/dustmaps     /dustmaps
-ln -sf /mnt/data/fsps         /fsps
-ln -sf /mnt/data/sbipp        /sbipp
-ln -sf /mnt/data/sbipp_phot   /sbipp_phot
+# The creation of symlinks should error if there is a non-symlink
+# file or folder where the symlink should be.
+if [[ ! -L "/data/cutout_cdn" ]]; then
+    ln -s /mnt/data/cutout_cdn   /data/cutout_cdn
+fi
+if [[ ! -L "/data/sed_output" ]]; then
+    ln -s /mnt/data/sed_output   /data/sed_output
+fi
+if [[ ! -L "/ghost_output" ]]; then
+    ln -s /mnt/data/ghost_output /ghost_output
+fi
+if [[ ! -L "/tns_staging" ]]; then
+    ln -s /mnt/data/tns_staging  /tns_staging
+fi
+if [[ ! -L "/transmission" ]]; then
+    ln -s /mnt/data/transmission /transmission
+fi
+if [[ ! -L "/dustmaps" ]]; then
+    ln -s /mnt/data/dustmaps     /dustmaps
+fi
+if [[ ! -L "/fsps" ]]; then
+    ln -s /mnt/data/fsps         /fsps
+fi
+if [[ ! -L "/sbipp" ]]; then
+    ln -s /mnt/data/sbipp        /sbipp
+fi
+if [[ ! -L "/sbipp_phot" ]]; then
+    ln -s /mnt/data/sbipp_phot   /sbipp_phot
+fi
