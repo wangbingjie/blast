dc714102d1727e14f70f5d8dfd175072988b0d77
T. Andrew Manning <manninga@illinois.edu>
2024-04-19 16:41:34 -0500

    Rebuilt requirements.txt from scratch, updating package versions
    Django updated to v5.0.4.
    astro_ghost temporarily installed from Git repo fork.

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,5 +1,4 @@
 # users/urls.py
-from django.conf.urls import include
-from django.conf.urls import url
+from django.urls import include, re_path
 
-urlpatterns = [url(r"^accounts/", include("django.contrib.auth.urls"))]
+urlpatterns = [re_path(r"^accounts/", include("django.contrib.auth.urls"))]
