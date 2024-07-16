2566f35793f03cf152ac18f95d0f97ea2a99bb53
T. Andrew Manning <manninga@illinois.edu>
2024-07-16 14:22:38 -0500

    Rename draft_test_utils.py back to test_utils.py

--- app/host/tests/test_utils.py
+++ app/host/tests/test_utils.py
@@ -1,5 +1,6 @@
 from astropy.coordinates import SkyCoord
 from django.test import TestCase
+from django.test import tag
 
 from ..catalog_photometry import catalog_photometry
 from ..catalog_photometry import filter_information
@@ -11,6 +12,7 @@ class CutoutDownloadTest(TestCase):
     def setUp(self):
         self.surveys = survey_list("host/data/survey_metadata.yml")
 
+    @tag('download')
     def test_cutout_download(self):
         """ "
         Test that cutout data can be downloaded.
@@ -28,6 +30,7 @@ class CatalogDownloadTest(TestCase):
     def setUp(self):
         self.catalogs = survey_list("host/data/catalog_metadata.yml")
 
+    @tag('download')
     def test_catalog_download(self):
         """
         Test that catalog data can be downloaded.
@@ -40,6 +43,7 @@ class CatalogDownloadTest(TestCase):
             with self.subTest(catalog=catalog.name):
                 self.assertTrue(catagog_data != None)
 
+    @tag('download')
     def test_filter_information_download(self):
         """
         Test that catalog filter data can be downloaded.
